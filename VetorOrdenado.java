c6f72a0462ff55ffdd9851a51d44150cc594b613
Daniela Linares <125320344+danilbalistero@users.noreply.github.com>
2023-05-26 22:00:30 -0300

    Exercicio Petri

--- VetorOrdenado.java
+++ VetorOrdenado.java
@@ -1,9 +1,8 @@
-
 import java.util.Scanner;
 
 public class Ordenacao {
     public static void main(String[] args) {
-        int tamanho;
+        int tamanho, aux;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Informe um valor inteiro e positivo");
@@ -15,20 +14,35 @@ public class Ordenacao {
             tamanho = scanner.nextInt();
         }
 
+        //parte do trabalho
         int [] vetor = new int[tamanho];
 
+        /*laço para preencher os vetores
+        enquanto i for menor q o tamanho declarado ele vai ficar repetindo*/
         for (int i = 0; i < tamanho ; i++) {
             System.out.println("Informe um valor para ser inserido na posição" + i + " do meu vetor:");
             int valor = scanner.nextInt();
             vetor[i] = valor;
-            
-        }
-        
-        for (int i = 0; i < tamanho; i++){
-            
+
         }
 
+        //Ordenação
+        for (int i = 0; i < tamanho - 1; i++){
+            for (int j = 0; j < tamanho - 1; j++) {
+                if (vetor[j] > vetor[j+1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+                 }
+            }
+        }
+
+
+        //Exibindo o vetor
+        for (int i = 0; i < tamanho; i++){
+            System.out.println("vetor[" + i + "] = " + vetor[i]);
+
+        }
 
     }
-}    
-    
+}
