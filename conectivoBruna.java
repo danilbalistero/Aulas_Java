fd4c7492c14945eb525499260fa756223e216c12
Daniela Linares <125320344+danilbalistero@users.noreply.github.com>
2023-08-06 17:37:28 -0300

    conectivoBruna.java
    Crie um programa ou pseudocódigo que, dadas duas proposições, decida o valor lógico delas utilizando os seguintes conectivos:
          Conjunção
          Disjunção
         Condicional
     O usuário precisa passar os valores lógicos das duas proposições e qual o conectivo gostaria de realizar a composição. O programa deve retornar o valor lógico.

--- conectivoBruna.java
+++ conectivoBruna.java
@@ -1,47 +1,51 @@
-package Bruna;
+package AtvBruna;
 
 import java.util.Scanner;
 
-public class TabelaVerdadeBruna {
-    public static void main(String[] args) {
+public class TabelaVerdade {
+        public static int avaliarProposicao(int proposicao1, int proposicao2, String conectivo) {
+            //Metodo para os valores logicos
 
-        Scanner scanner = new Scanner(System.in);
-        int conectivo = 0;
+            //equals para comparar os valores
+            if (conectivo.equals("conjunção")) {
+                return (proposicao1 == 1 && proposicao2 == 1) ? 1 : 0; //
+            } else if (conectivo.equals("disjunção")) {
+                return (proposicao1 == 1 || proposicao2 == 1) ? 1 : 0;
+            } else if (conectivo.equals("condicional")) {
+                return (!(proposicao1 == 1) || proposicao2 == 1) ? 1 : 0;
+            } else {
+                return -1; // valor inválido
+            }
+        }
 
-        System.out.println("Informe um valor logico de A utilizando: [1] VERDADEIRO ou [0] FALSO");
-        boolean a = scanner.nextBoolean();
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Informe o valor logico de B utilizando: [1] VERDADEIRO ou [0] FALSO");
-        boolean b = scanner.nextBoolean();
+            System.out.print("Digite o valor lógico da proposição 1 ([1] para VERDADEIRO ou [0] para FALSO): ");
+            int proposicao1 = scanner.nextInt();
+
+            System.out.print("Digite o valor lógico da proposição 2 ([1] para VERDADEIRO ou [0] para FALSO): ");
+            int proposicao2 = scanner.nextInt();
 
             System.out.println("           ╔════════════════════════════════╗                       ");
             System.out.println("           ║ ʕ•́ᴥ•̀ʔ Menu de Conectivos ʕ•́ᴥ•̀ʔ ║  ");
             System.out.println("╔══════════╩════════════════════════════════╩═══════╗");
-            System.out.println("║ 1 - Conjunção                                     ║");
-            System.out.println("║ 2 - Disjunção                                     ║");
-            System.out.println("║ 3 - Condicional                                   ║");
+            System.out.println("║                     Conjunção                     ║");
+            System.out.println("║                     Disjunção                     ║");
+            System.out.println("║                    Condicional                    ║");
             System.out.println("╚═══════════════════════════════════════════════════╝");
+            System.out.println("Digite qual conectivo que deseja utilizar");
+            String conectivo = scanner.next();
 
-        System.out.println(" ");
-        System.out.println("           Informe a operação desejada!    ");
-        conectivo = scanner.nextInt();
-
-        boolean resultado = false;
-
-        switch (conectivo){
-            case 1: //conjunção
-                resultado = a && b;
-                break;
-            case 2: //disjunção
-                resultado = a || b;
-                break;
-            case 3: //condicional
-                resultado = !a || b;
-                break;
-            default:
-                System.out.println("O conectivo é invalido!!");
-                System.exit(0);
+            int resultado = avaliarProposicao(proposicao1, proposicao2, conectivo);
+            if (resultado == 1) {
+                System.out.println("O resultado lógico da composição é: verdadeiro");
+            } else if (resultado == 0) {
+                System.out.println("O resultado lógico da composição é: falso");
+            } else {
+                System.out.println("Conectivo inválido!");
+            }
         }
-        System.out.println("O valor logico do conectivo [" + conectivo + "] é igual a: " + resultado);
     }
-}
+
+
